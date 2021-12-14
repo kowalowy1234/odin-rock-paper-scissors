e5ec449a7b776b66a5c95feb84e3e3dadf9e9193
kowalowy1234 <kowalowy1234@gmail.com>
2021-12-14 20:14:49 +0100

    Wrote a function returning computer generated choice of a weapon

--- javascript/game.js
+++ javascript/game.js
@@ -1,1 +1,16 @@
 console.log('Hello world!');
+
+function computerGame(){
+    const choiceNumber = Math.floor(Math.random() * 3) + 1
+    let weapon = '';
+    if(choiceNumber === 1) {
+        weapon = 'ROCK';
+    } else if (choiceNumber === 2) {
+        weapon = 'PAPER';
+    } else {
+        weapon = 'SCISSORS';
+    }
+    console.log(choiceNumber);
+    return weapon;
+}
+
