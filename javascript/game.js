11794326c8cabe489a7241891fb8565be4143f2d
kowalowy1234 <kowalowy1234@gmail.com>
2021-12-14 21:06:00 +0100

    Created a function game() that plays 5 rounds of rock-paper-scissors

--- javascript/game.js
+++ javascript/game.js
@@ -26,6 +26,50 @@ function playRound(playerSelection, computerSelection){
     return winner;
 }
 
+function game(){
+    let playerSelection;
+    let computerSelection;
 
+    console.log('Let\'s play a game of ROCK-PAPER-SCISSORS!');
+    console.log('First to 5 points wins...');
 
-playRound(playerSelection, computerSelection);
+    let playerScore = 0;
+    let computerScore = 0;
+
+    let round = 0;
+    let roundWinner;
+
+    while(round < 5) {
+        playerSelection = prompt('Write either \'Rock\',  \'Paper\' or \'Scissors\'');
+        if (playerSelection === null){
+            console.log('Oh come on... Play it!');
+            break;
+        } else {
+            computerSelection = computerGame();
+            roundWinner = playRound(playerSelection, computerSelection);
+            
+            if (roundWinner === 1) {
+                playerScore += 1;
+                console.log('Player wins this round!')
+                console.log(`player Score: ${playerScore} - ${computerScore} :computer Score`);
+            } else if (roundWinner === 2) {
+                computerScore += 1;
+                console.log('Computer wins this round!')
+                console.log(`player Score: ${playerScore} - ${computerScore} :computer Score`);
+            } else {
+                console.log('Looks like this one\'s a draw!')
+                console.log(`player Score: ${playerScore} - ${computerScore} :computer Score`);
+            }
+            round += 1;
+        }
+    }
+    
+    if (playerScore === computerScore) {
+        console.log('DRAW')
+    } else {
+        (playerScore > computerScore) ? console.log('PLAYER WINS!!!') : console.log('COMPUTER WINS!!!');
+    }
+    
+}
+
+game();
