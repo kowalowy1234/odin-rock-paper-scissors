24247e2d8b3d82ac2b670b1ec4591e32f9f57056
kowalowy1234 <kowalowy1234@gmail.com>
2021-12-14 20:37:35 +0100

    Created a function playRound() that plays one round of rock-paper-scissors

--- javascript/game.js
+++ javascript/game.js
@@ -1,9 +1,31 @@
-console.log('Hello world!');
+const weapons = ['Rock', 'Paper', 'Scissors'];
 
 function computerGame(){
-
     const choiceNumber = Math.floor(Math.random() * 3)
-    const weapons = ['Rock', 'Paper', 'Scissors'];
     return weapons[choiceNumber];
 }
 
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toUpperCase();
+    computerSelection = computerSelection.toUpperCase();
+    let winner;
+    console.log(`player: ${playerSelection} - ${computerSelection} :computer`);
+    if (playerSelection === computerSelection) {
+        winner = 0;
+    } else {
+        if (playerSelection === 'ROCK' && computerSelection === 'SCISSORS'){
+            winner = 1;
+        } else if (playerSelection === 'PAPER' && computerSelection === 'ROCK'){
+            winner = 1;
+        } else if (playerSelection === 'SCISSORS' && computerSelection === 'PAPER'){
+            winner = 1;
+        } else {
+            winner = 2;
+        }
+    }
+    return winner;
+}
+
+
+
+playRound(playerSelection, computerSelection);
