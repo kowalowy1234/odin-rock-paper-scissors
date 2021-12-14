dae8ab191fdb56abb30b95aa24eb77cd0377428d
kowalowy1234 <kowalowy1234@gmail.com>
2021-12-14 20:21:32 +0100

    Rewrote computerGame() function to a shorter form without conditionals

--- javascript/game.js
+++ javascript/game.js
@@ -1,16 +1,9 @@
 console.log('Hello world!');
 
 function computerGame(){
-    const choiceNumber = Math.floor(Math.random() * 3) + 1
-    let weapon = '';
-    if(choiceNumber === 1) {
-        weapon = 'ROCK';
-    } else if (choiceNumber === 2) {
-        weapon = 'PAPER';
-    } else {
-        weapon = 'SCISSORS';
-    }
-    console.log(choiceNumber);
-    return weapon;
+
+    const choiceNumber = Math.floor(Math.random() * 3)
+    const weapons = ['Rock', 'Paper', 'Scissors'];
+    return weapons[choiceNumber];
 }
 
